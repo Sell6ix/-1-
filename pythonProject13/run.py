babe245a9bfaa2adf9af1b1e02b4129ef8f1388d
Sell6ix <110632259+Sell6ix@users.noreply.github.com>
2022-09-11 16:01:01 +0300

    Add files via upload

---
+++ pythonProject13/run.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+from posts_views import posts_blueprint
+from bookmarks_views import boookmarks_blueprint
+
+app = Flask(__name__)
+
+
+app.register_blueprint(posts_blueprint)
+app.register_blueprint(boookmarks_blueprint)
+
+
+if __name__ == "__main__":
+    app.run()
